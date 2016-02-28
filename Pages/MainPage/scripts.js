f1563c14352ada485543ec5775782cca363b6e9d
Evgeniy <Micromagicmen@gmail.com>
2016-02-28 14:30:14 +0300

    Главная страничка

---
+++ Pages/MainPage/scripts.js
@@ -0,0 +1,9 @@
+function onSignInClicked() {
+    "use strict";
+    alert("Здесь будет выводиться диалоговое окно для регистрации");
+}
+
+function onLogInClicked() {
+    "use strict";
+    alert("Здесь будет выводиться диалоговое окно для входа на сайт");
+}
