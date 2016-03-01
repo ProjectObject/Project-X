6a0ae190c07dab7bdc6634e63c26cd657128fa74
Evgeniy <Micromagicmen@gmail.com>
2016-03-01 12:51:08 +0300

    register

--- Pages/MainPage/backChange.js
+++
@@ -1,8 +0,0 @@
-var background = 0;
-var backChange = function() {
-    background++;
-    var pictures = ['slides/slide-1.png', 'slides/slide-2.png', 'slides/slide-3.png', 'slides/slide-4.png'];
-    document.body.style.background = 'url(' +pictures[background] + ')';
-    background = background == 3 ? -1 : background;
-};
-setInterval(backChange, 6000);
--- Pages/MainPage/scripts.js
+++
@@ -1,9 +0,0 @@
-function onSignInClicked() {
-    "use strict";
-    alert("Здесь будет выводиться диалоговое окно для регистрации");
-}
-
-function onLogInClicked() {
-    "use strict";
-    alert("Здесь будет выводиться диалоговое окно для входа на сайт");
-}
