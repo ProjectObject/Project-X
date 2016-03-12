5fcd646f33f9e87c40de83cb59728e9f97be3117
project_one <Projectgit007@gmail.com>
2016-03-12 20:28:21 +0300

    events

--- Pages/JS/mainPage.js
+++ Pages/JS/mainPage.js
@@ -1,5 +1,3 @@
-var volume = true;
-
 $(document).ready(function(){
     $("#dialog").hide();
 });
@@ -10,18 +8,6 @@ function openDialog(){
 
 function closeDialog(){
     $("#dialog").fadeOut();
-}
-
-function switchVolume() {
-    if(!volume) {
-         $("#video-background").prop('muted', false);
-         $("#volume").css("background", "url(CSS/slides/sound_on.png)");
-        volume = !volume;
-    } else {
-        $("#video-background").prop('muted', true);
-        $("#volume").css("background", "url(CSS/slides/sound_off.png)");
-        volume = !volume;
-    }
 }
 
 
