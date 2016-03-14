b1314216363b1a9c35b6d0716d0ae9f05f0b221a
project_one <Projectgit007@gmail.com>
2016-03-14 18:33:07 +0300

    slider

--- Pages/JS/mainPage.js
+++ Pages/JS/mainPage.js
@@ -1,5 +1,13 @@
+var slideIndex;
+var slideTime = 8000;
+var slideArray = $(".slide");
+
 $(document).ready(function(){
     $("#dialog").hide();
+    slideIndex = 0;
+	slideArray.css({"position" : "absolute",
+	"top":'0', "left": '0'}).hide();
+	slideArray.eq(slideIndex).show();
 });
 
 function openDialog(){
@@ -9,6 +17,15 @@ function openDialog(){
 function closeDialog(){
     $("#dialog").fadeOut();
 }
+
+var changeSlide = function() {
+	slideArray.eq(slideIndex).fadeOut(2500);
+	slideIndex++;
+	slideIndex = slideIndex == 5 ? 0 : slideIndex;
+	slideArray.eq(slideIndex).fadeIn(2500);
+}
+
+setInterval(changeSlide, slideTime);
 
 
 
